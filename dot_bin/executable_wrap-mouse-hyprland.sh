6ad7b8271fb4f459f0f8b53f844add0d70177ef0
Anthony Farquharson <scooterdelta@gmail.com>
2024-06-08 23:07:08 +0100

    Updates script to update output for main PC

---
+++ dot_bin/executable_wrap-mouse-hyprland.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+state="${HOME}/.config/togglemonitorlock"
+booleanvalue="false"
+
+if [[ -f ${state} ]]; then
+     booleanvalue=$(cat ${state})
+fi
+
+if [[ ${booleanvalue} == "true" ]]; then
+     wlr-randr --output DP-3 --pos 3440,0
+     echo "false" > ${state}
+else
+     wlr-randr --output DP-3 --pos 3440,2000
+     echo "true" > ${state}
+fi
