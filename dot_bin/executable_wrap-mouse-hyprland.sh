eb9deb1c92a021ca5b6f4ffa2acf41b48e0469b3
Anthony Farquharson <scooterdelta@gmail.com>
2024-06-09 18:27:24 +0100

    Updates monitor shift script

--- dot_bin/executable_wrap-mouse-hyprland.sh
+++ dot_bin/executable_wrap-mouse-hyprland.sh
@@ -8,9 +8,11 @@ if [[ -f ${state} ]]; then
 fi
 
 if [[ ${booleanvalue} == "true" ]]; then
+     wlr-randr --output DP-1 --pos 0,0
      wlr-randr --output DP-3 --pos 3440,0
      echo "false" > ${state}
 else
+     wlr-randr --output DP-1 --pos 0,0
      wlr-randr --output DP-3 --pos 3440,2000
      echo "true" > ${state}
 fi
